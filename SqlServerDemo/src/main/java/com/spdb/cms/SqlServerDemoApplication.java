c29ce49835cff465dcd0d751a8f4529176140da8
Administrator <Administrator@PC-20171012LLPK>
2019-01-02 18:15:35 +0800

    this is a test

---
+++ SqlServerDemo/src/main/java/com/spdb/cms/SqlServerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.spdb.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlServerDemoApplication.class, args);
+	}
+}
